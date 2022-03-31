1ce315cfd5b1579c7e16b316f498c0cf5b396ec2
TravelByBeaut <ebeaumont77@gmail.com>
2022-03-31 18:16:31 +0100

    First form attempt

---
+++ index.js
@@ -0,0 +1,25 @@
+function ComputeTea() {
+	const isSweet = document.body.querySelector('#sweet').checked;
+  const isFloral = document.body.querySelector('#floral').checked;
+  const isBitter = document.body.querySelector('#bitter').checked;
+  const isDelicate = document.body.querySelector('#delicate').checked;
+  const isSmoky = document.body.querySelector('#smoky').checked;
+  
+  if(isSweet) {
+  	  console.log('camomile');
+  } else if(isFloral) {
+      console.log('jasmine');
+  } else if(isBitter) {
+      console.log('green tea');
+  } else if (isDelicate) {
+      console.log('white tea')
+  } else if (isSmoky) {
+      console.log('lapsong souchong')
+  } else {
+  	console.log('please select a flavour')
+  }
+  }
+console.log = function(message) {
+    document.getElementById('result').innerHTML = message;
+};
+console.log('');
