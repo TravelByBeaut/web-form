7147885f73cb24d9a2183c2eca89621caf77e4eb
TravelByBeaut <ebeaumont77@gmail.com>
2022-03-31 21:26:16 +0100

    pop up better

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-function ComputeTea() {
-	const isSweet = document.body.querySelector('#sweet').checked;
-  const isFloral = document.body.querySelector('#floral').checked;
-  const isBitter = document.body.querySelector('#bitter').checked;
-  const isDelicate = document.body.querySelector('#delicate').checked;
-  const isSmoky = document.body.querySelector('#smoky').checked;
-  
-  function computeTea() {
-	const resultsDiv = document.body.querySelector('#results-container');
-  resultsDiv.hidden = false;
-}
-
-function hideResults() {
-	const resultsDiv = document.body.querySelector('#results-container');
-  resultsDiv.hidden = true;
-}
-
-  if(isSweet) {
-  	  console.log('camomile');
-  } else if(isFloral) {
-      console.log('jasmine');
-  } else if(isBitter) {
-      console.log('green tea');
-  } else if (isDelicate) {
-      console.log('white tea')
-  } else if (isSmoky) {
-      console.log('lapsong souchong')
-  } else {
-  	console.log('please select a flavour')
-  }
-  }
-console.log = function(message) {
-    document.getElementById('result').innerHTML = message;
-};
-console.log('');
+function computeTea() {
+    computeFlavour();
+    showResults();
+    message();
+    }
+    
+    function computeFlavour() {
+        const isSweet = document.body.querySelector('#sweet').checked;
+      const isFloral = document.body.querySelector('#floral').checked;
+      const isBitter = document.body.querySelector('#bitter').checked;
+      const isDelicate = document.body.querySelector('#delicate').checked;
+      const isSmoky = document.body.querySelector('#smoky').checked;  
+      if(isSweet) {
+            console.log('camomile');
+      } else if(isFloral) {
+          console.log('jasmine' + img);
+      } else if(isBitter) {
+          console.log('green tea');
+      } else if (isDelicate) {
+          console.log('white tea')
+      } else if (isSmoky) {
+          console.log('lapsong souchong')
+      } else {
+          console.log('please select a flavour')
+      }
+    }
+      
+          function showResults() {
+        const resultsDiv = document.body.querySelector('#results-container');
+      resultsDiv.hidden = false;
+      }
+    
+    function hideResults() {
+        const resultsDiv = document.body.querySelector('#results-container');
+      resultsDiv.hidden = true;
+    }
+    function displayMesage(message) {
+        document.getElementById('results').innerHTML = message;
+      }
+    
+    
